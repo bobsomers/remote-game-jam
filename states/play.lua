dc23ebcda041d9a329cf6ceceb73629b35e3cfcb
Bob Somers <bob@bobsomers.com>
2012-03-17 03:36:02 -0700

    Collidable tile loading.

--- states/play.lua
+++ states/play.lua
@@ -18,7 +18,30 @@ function PlayState:init()
 
     -- Load the tile map and set up solid tiles.
     MapLoader.path = "maps/"
-    self.map = MapLoader.load("test2.tmx")
+    self.map = MapLoader.load("test.tmx")
+
+    -- Setup passive collision shapes for tiles in the collidable layer.
+    local layer = map.tl["collidable"]
+    for x = 1, map.width do
+        for y = 1, map.height do
+            if layer.tileData[y] then
+                local tile = map.tiles[layer.tileData[y][x]]
+                if tile.properties.solid then
+                    local shape = self.collider:addRectangle(
+                        (x - 1) * 32, (y - 1) * 32,
+                        32, 32
+                    )
+                    shape.kind = "tile"
+                    self.collider:addToGroup("world", shape)
+                    self.collider:setPassive(shape)
+                elseif tile.properties.rampUp then
+                    -- TODO
+                elseif tile.properties.rampDown then
+                    -- TODO
+                end
+            end
+        end
+    end
 
     -- Reset transient game state.
     self:reset()
