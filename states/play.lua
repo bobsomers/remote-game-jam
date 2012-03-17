37944e7011fb1ccabb892263a3d0e3ef73df4869
Bob Somers <bob@bobsomers.com>
2012-03-17 03:06:11 -0700

    Entity manager.

--- states/play.lua
+++ states/play.lua
@@ -1,5 +1,7 @@
 local Gamestate = require "hump.gamestate"
 local MapLoader = require "AdvTiledLoader.Loader"
+local Collider = require "hardoncollider"
+local EntityManager = require "entities.manager"
 local Constants = require "constants"
 local Player = require "entities.player"
 local PubMate = require "entities.pubmate"
@@ -8,6 +10,13 @@ local Bro = require "entities.bro"
 local PlayState = Gamestate.new()
 
 function PlayState:init()
+    -- Set up the collision detection engine.
+    self.collider = Collider(100, function(dt, shape1, shape2, mtvX, mtvY)
+        -- Just forwards to self instance through closure.
+        self:collide(dt, shape1, shape2, mtvX, mtvY)
+    end)
+
+    -- Load the tile map and set up solid tiles.
     MapLoader.path = "maps/"
     self.map = MapLoader.load("test2.tmx")
 
@@ -17,10 +26,12 @@ end
 
 function PlayState:reset()
     self.lastFpsTime = 0
+    self.entities = EntityManager()
 end
 
 function PlayState:update(dt)
-    -- TODO
+    -- Update all entities.
+    self.entities:update(dt)
 
     -- Update FPS in window title (if DEBUG MODE is on).
     if Constants.DEBUG_MODE then
@@ -39,6 +50,11 @@ end
 
 function PlayState:draw()
     self.map:draw()
+    self.entities:draw()
+end
+
+function PlayState:collide(dt, shape1, shape2, mtvX, mtvY)
+    -- TODO
 end
 
 return PlayState
